6a9d552661473d1742f199b753a0ba529bccbf23
prasad98765 <pnchaudhari1996@gmail.com>
2019-12-06 18:48:54 +0530

    Added: Part Time Hour

--- EmployeeWage.sh
+++ EmployeeWage.sh
@@ -3,7 +3,8 @@
 echo "Welcome to Employee Wage Computation Program"
 WAGE_PER_HOUR=20
 WORKING_HOUR=8
-
+HALF_DAY_WORKING_HOUR=4
+FULL_DAY_WORKING_HOUR=8
 	employee=$((RANDOM%2))
 
 	if [[ $employee -eq 0 ]]
