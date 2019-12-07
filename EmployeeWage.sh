89946346e84d4e55914c4ed3ea12012436159c88
prasad98765 <pnchaudhari1996@gmail.com>
2019-12-07 13:54:55 +0530

    Added : Total Working Hours

--- EmployeeWage.sh
+++ EmployeeWage.sh
@@ -11,33 +11,32 @@ ABSENT=2
 WORKING_DAY_PER_MONTH=20
 DAILY_WAGE=0
 DAY=1
-	employee=$((RANDOM%2))
-	if [[ $employee -eq 0 ]]
-	then
-		echo "Employee Absent"
-	else
-		echo "Emoloyee Present"
-		while [[ $DAY -lt 20 ]]
+TOTAL_WORKING_HRS=100
+TOTAL_HRS=0
+		while [[ $TOTAL_HRS -lt $TOTAL_WORKING_HRS &&  $DAY -lt 20 ]]
 		do 
 		 	partFullTime=$((RANDOM%3))
 			case $partFullTime in
 			$PART_TIME)
-				   echo "He is work Parttime"
-				   echo "Working hour is" $PART_DAY_WORKING_HOUR
+				   #echo "He is work Parttime"
+				   #echo "Working hour is" $PART_DAY_WORKING_HOUR
 				   DAILY_WAGE=$(( $WAGE_PER_HOUR * $PART_DAY_WORKING_HOUR ))
+					TOTAL_HRS=$(( $TOTAL_HRS + $PART_DAY_WORKING_HOUR ))
 				   ;;
 			$FULL_TIME)
-				   echo "He is work FullTime"
-				   echo "Working hour is" $FULL_DAY_WORKING_HOUR
+				   #echo "He is work FullTime"
+				   #echo "Working hour is" $FULL_DAY_WORKING_HOUR
 				   DAILY_WAGE=$(( $WAGE_PER_HOUR * $FULL_DAY_WORKING_HOUR ))
+					TOTAL_HRS=$(( $TOTAL_HRS + $FULL_DAY_WORKING_HOUR ))
 				   ;;
 			$ABSENT)
 					echo "Employee Absent"
 		esac
 		DAY=$(($DAY + 1))
+		echo "Total Employee Hrs" $TOTAL_HRS 
 		echo "Daily Wage $DAY" $DAILY_WAGE
 		done
-	fi
+	
 
 
 
