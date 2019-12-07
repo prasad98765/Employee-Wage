0abac8177da65270b1407430b20bdadf4b6926c5
prasad98765 <pnchaudhari1996@gmail.com>
2019-12-07 15:25:53 +0530

    Modified : To function calculate Wage and Hrs

--- EmployeeWage.sh
+++ EmployeeWage.sh
@@ -2,41 +2,38 @@
 
 echo "Welcome to Employee Wage Computation Program"
 WAGE_PER_HOUR=20
-WORKING_HOUR=8
-PART_DAY_WORKING_HOUR=4
-FULL_DAY_WORKING_HOUR=8
+#WORKING_HOUR=8
 PART_TIME=0
 FULL_TIME=1
 ABSENT=2
 WORKING_DAY_PER_MONTH=20
-DAILY_WAGE=0
-DAY=1
 TOTAL_WORKING_HRS=100
 TOTAL_HRS=0
-		while [[ $TOTAL_HRS -lt $TOTAL_WORKING_HRS &&  $DAY -lt 20 ]]
-		do 
-		 	partFullTime=$((RANDOM%3))
-			case $partFullTime in
-			$PART_TIME)
-				   #echo "He is work Parttime"
-				   #echo "Working hour is" $PART_DAY_WORKING_HOUR
-				   DAILY_WAGE=$(( $WAGE_PER_HOUR * $PART_DAY_WORKING_HOUR ))
-					TOTAL_HRS=$(( $TOTAL_HRS + $PART_DAY_WORKING_HOUR ))
-				   ;;
-			$FULL_TIME)
-				   #echo "He is work FullTime"
-				   #echo "Working hour is" $FULL_DAY_WORKING_HOUR
-				   DAILY_WAGE=$(( $WAGE_PER_HOUR * $FULL_DAY_WORKING_HOUR ))
-					TOTAL_HRS=$(( $TOTAL_HRS + $FULL_DAY_WORKING_HOUR ))
-				   ;;
-			$ABSENT)
-					echo "Employee Absent"
-		esac
+
+
+function partFulltimeWork(){
+partFullTime=$1
+case $partFullTime in
+      $PART_TIME)
+  					DayWorkingHour=4
+               ;;
+      $FULL_TIME)
+             	DayWorkingHour=8
+               ;;
+      $ABSENT)
+               DayWorkingHour=0
+      esac
+echo $DayWorkingHour
+}
+while [[ TOTAL_HRS -lt TOTAL_WORKING_HRS && DAY -lt WORKING_DAY_PER_MONTH ]]
+do
+		partFullTime=$((RANDOM%3))
+		HRS="$( partFulltimeWork $partFullTime )"
+		TOTAL_HRS=$(( $HRS + $TOTAL_HRS ))
 		DAY=$(($DAY + 1))
-		echo "Total Employee Hrs" $TOTAL_HRS 
+		echo "Total Employee Hrs" $TOTAL_HRS
 		echo "Daily Wage $DAY" $DAILY_WAGE
-		done
-	
+done
 
 
 
