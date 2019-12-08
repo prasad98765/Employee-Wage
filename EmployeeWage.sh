cdcf72ddcf48a89b7707b82971e4b8b31c850f5a
prasad98765 <58411730+prasad98765@users.noreply.github.com>
2019-12-08 11:11:59 +0530

    Update EmployeeWage.sh

--- EmployeeWage.sh
+++ EmployeeWage.sh
@@ -1,10 +1,10 @@
 #!/bin/bash -x
 
 echo "Welcome to Employee Wage Computation Program"
+
 WAGE_PER_HOUR=20
 WORKING_HOUR=8
-
-	employee=$((RANDOM%2))
+employee=$((RANDOM%2))
 
 	if [[ $employee -eq 0 ]]
 	then
