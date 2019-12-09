bf1f950e7d510b2b7566d9cad72deb5af17b21d2
prasad98765 <pnchaudhari1996@gmail.com>
2019-12-09 10:26:51 +0530

    Added : Calculate Total Wage Store in Array

---
+++ ArithmaticOpr.sh
@@ -0,0 +1,6 @@
+#!/bin/bash
+
+echo "Take 3 Input"
+read a
+read b
+read c
--- EmployeeWage.sh
+++ EmployeeWage.sh
@@ -7,15 +7,15 @@ PART_TIME=0
 FULL_TIME=1
 ABSENT=2
 WORKING_DAY_PER_MONTH=20
-TOTAL_WORKING_HRS=100
 COUNT=0
+TOTAL_HRS=0
 
 
 function partFulltimeWork(){
 partFullTime=$1
 case $partFullTime in
       $PART_TIME)
-  					DayWorkingHour=4
+  		DayWorkingHour=4
                ;;
       $FULL_TIME)
              	DayWorkingHour=8
@@ -25,21 +25,21 @@ case $partFullTime in
       esac
 echo $DayWorkingHour
 }
-while [[	DAY -lt WORKING_DAY_PER_MONTH ]]
+while [[ DAY -lt WORKING_DAY_PER_MONTH ]]
 do
 		partFullTime=$((RANDOM%3))
 		HRS="$( partFulltimeWork $partFullTime )"
 		#EMPLOYEE_TOTAL_HRS=$(( $HRS + $EMPLOYEE_TOTAL_HRS )) 
 		dailyWage=$(( $WAGE_PER_HOUR * $HRS ))
 		dailyWage[((COUNT++))]=$dailyWage
-      hrs=$(( $TOTAL_HRS + $dailyWage ))
-		totalHrs[((COUNT++))]=$hrs 
+		Total_Wage=$(( $Total_Wage + $dailyWage ))
+		TOTAL_HRS=$(( $TOTAL_HRS + $HRS ))
+		totalHrs[(($COUNT))]=$TOTAL_HRS 
 		DAY=$(($DAY + 1))
 done
-
-
 echo ${dailyWage[@]}
 echo ${totalHrs[@]}
-
+echo "Total Wage" $Total_Wage
+echo "Total Hrs" $TOTAL_HRS
 
 
